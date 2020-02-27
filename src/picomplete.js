e0c0530e286fb99a7f1506469c19b0fbe2296a37
Kadir <kbbciloglu@gmail.com>
2020-02-27 11:34:05 +0300

    General fix patch

--- src/picomplete.js
+++ src/picomplete.js
@@ -24,15 +24,17 @@ class PickleComplate {
      */
     staticEvents() {
         // key up event
-        document.querySelectorAll(this.config.target + ' input').forEach(e => {
+        document.querySelectorAll(this.config.target+' input').forEach(e => {
             e.addEventListener('keyup', el => {
-                if (el.target.value.trim().length > 0) {
+                if(el.target.value.trim().length>0){
                     this.element = el.target;
                     this.closeAllLists();
                     //if anything is match show list
                     setTimeout(() => {
                         this.getSuggests(el.target);
                     }, 30);
+                }else{
+                    this.closeAllLists();
                 }
             });
         });
@@ -46,10 +48,10 @@ class PickleComplate {
         //listen item clicks
         document.querySelectorAll(this.config.target).forEach(e => {
             e.addEventListener('click', el => {
-                if (el.target.classList.contains('picomplete-item')) {
-                    if (this.config.clickCallback !== undefined && this.config.clickCallback !== null) {
+                if(el.target.classList.contains('picomplete-item')){
+                    if(this.config.clickCallback !== undefined && this.config.clickCallback !== null){
                         //send target element and node data
-                        this.config.clickCallback(this.element, this.container[el.target.getAttribute('data-index')])
+                        this.config.clickCallback(this.element,this.container[el.target.getAttribute('data-index')])
                     }
                 }
             });
@@ -79,7 +81,7 @@ class PickleComplate {
         this.sug_div = document.createElement('DIV');
         this.sug_div.classList.add('picomplete-items');
         //check container type
-        if (this.config.type === 'server') {
+        if(this.config.type === 'server'){
             await this.getData(el.value.toLowerCase());
         }
         //for each item in container
@@ -90,9 +92,9 @@ class PickleComplate {
                 //set class
                 item.classList.add('picomplete-item');
                 //set value
-                item.setAttribute('data-value', this.container[i].value);
+                item.setAttribute('data-value',this.container[i].value);
                 //set value
-                item.setAttribute('data-index', i);
+                item.setAttribute('data-index',i);
                 //set text
                 item.innerHTML = this.container[i].text;
                 //add item to list
@@ -100,7 +102,7 @@ class PickleComplate {
             }
         }
         //add list to input 
-        this.element.parentNode.appendChild(this.sug_div);
+        if(this.sug_div !== null) this.element.parentNode.appendChild(this.sug_div);
     }
 
 
@@ -109,10 +111,9 @@ class PickleComplate {
      * this method will send request to given parameters and return list of results
      * @param {string} value 
      */
-    async getData(value) {
-        this.container = [];
+    async getData(value){
         //define if parameters is not defined
-        if (this.ajax.param === undefined) {
+        if(this.ajax.param===undefined){
             this.ajax.param = {};
         }
         //set value to params
@@ -122,11 +123,12 @@ class PickleComplate {
             url: this.ajax.url,
             data:this.ajax.param
         }).then(rsp => {
-            if (rsp.length > 0) {
-                for (let i = 0; i < rsp.length; i++) {
+            this.container = [];
+            if(rsp.length>0){
+                for (let i = 0; i <rsp.length; i++) {
                     this.container.push({
-                        value: rsp[i][this.ajax.value],
-                        text: rsp[i][this.ajax.text],
+                        value : rsp[i][this.ajax.value],
+                        text : rsp[i][this.ajax.text],
                     })
                 }
             }
@@ -140,7 +142,7 @@ class PickleComplate {
      * system request method
      * @param {json object} rqs 
      */
-    async request(rqs, file = null) {
+     async request(rqs, file = null) {
         let fD = new FormData();
         let rsp;
         let url_params = [];
@@ -155,14 +157,14 @@ class PickleComplate {
             if (file !== null) {
                 fD.append('file', file, file.name);
             }
-        } else {
-            if (!rqs['url'].includes("&")) {
-                rqs['url'] += '&';
+        }else{
+            if(!rqs['url'].includes("&")){
+                rqs['url']+='&';
             }
             for (let key in rqs['data']) {
-                url_params.push(key + '=' + rqs['data'][key]);
+                url_params.push(key+'='+rqs['data'][key]);
             }
-            rqs['url'] += url_params.join('&');
+            rqs['url']+=url_params.join('&');
         }
 
         rsp = await fetch(rqs['url'], op);
